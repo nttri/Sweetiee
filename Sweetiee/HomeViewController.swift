8aa45bf8805523f06094de1391729fba55cf2cd4
tringuyen <bem0302@gmail.com>
2022-04-19 23:43:45 +0700

    update background video

--- Sweetiee/HomeViewController.swift
+++ Sweetiee/HomeViewController.swift
@@ -6,13 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
+    
+    private var player: AVPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playBackgroundVideo()
     }
 
-
+    //MARK: Video background
+    private func playBackgroundVideo() {
+        guard let path = Bundle.main.path(forResource: "background", ofType: ".mp4") else {
+            return
+        }
+        
+        player = AVPlayer(url: URL(fileURLWithPath: path))
+        player.actionAtItemEnd = .none
+        
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = view.frame
+        layer.videoGravity = .resizeAspectFill
+        layer.opacity = 0.6
+        view.layer.insertSublayer(layer, at: 0)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(playerDidReachEnd),
+                                               name: .AVPlayerItemDidPlayToEndTime,
+                                               object: player.currentItem)
+        player.seek(to: .zero)
+        player.play()
+        player.isMuted = true
+    }
+    
+    @objc private func playerDidReachEnd() {
+        player.seek(to: .zero)
+    }
 }
