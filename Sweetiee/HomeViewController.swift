154dee3f111f1bd0ebe630a699fa28e7e4a3410e
tringuyen <bem0302@gmail.com>
2022-04-19 22:53:59 +0700

    init project

---
+++ Sweetiee/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Sweetiee
+//
+//  Created by tringuyen on 19/04/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
